90622597a80ed420d1ee8ef6258cb97592f66792
SujiKarthick <thanvesh2402@gmail.com>
2022-11-23 13:34:48 +0530

    New Project

---
+++ src/org/git/Sample.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class Sample {
+	private void add() {
+		int a = 10, b = 20;
+		System.out.println(a + b);
+	}
+
+	public static void main(String[] args) {
+		Sample a = new Sample();
+		a.add();
+	}
+
+}
